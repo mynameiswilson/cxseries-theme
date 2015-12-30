30e3b4a91a4cf99ff0961a18727d4c02822994c7
Ben Wilson <ben@thelocust.org>
2015-12-29 22:26:59 -0500

    results page
    - created “results” page with file listing capabilities

---
+++ page-results.php
@@ -0,0 +1,8 @@
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+
+
+	<?php cxseries_list_results(); ?>
+
+<?php endwhile; ?>
