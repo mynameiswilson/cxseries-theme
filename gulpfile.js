ce9f80cb1f11cf59deef966945d71e9b98b1526d
Ben Wilson <ben@thelocust.org>
2016-12-29 18:20:41 -0500

    update gulp file to sage 8.5.0

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var jshint       = require('gulp-jshint');
 var lazypipe     = require('lazypipe');
 var less         = require('gulp-less');
 var merge        = require('merge-stream');
-//var cssNano      = require('gulp-cssnano');
+var cssNano      = require('gulp-cssnano');
 var plumber      = require('gulp-plumber');
 var rev          = require('gulp-rev');
 var runSequence  = require('run-sequence');
@@ -104,9 +104,9 @@ var cssTasks = function(filename) {
         'opera 12'
       ]
     })
-/*    .pipe(cssNano, {
+    .pipe(cssNano, {
       safe: true
-  })*/
+    })
     .pipe(function() {
       return gulpif(enabled.rev, rev());
     })
