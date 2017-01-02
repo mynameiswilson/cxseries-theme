a36238b4595eab7cb53d4dfa2ba7488262a82769
Ben Wilson <ben@thelocust.org>
2017-01-01 20:54:46 -0500

    wpcs updates

--- page-results.php
+++ page-results.php
@@ -1,7 +1,19 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+<?php
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-  <?php cxseries_list_results(); ?>
+while ( have_posts() ) :
+	the_post(); ?>
+    <?php get_template_part( 'templates/page', 'header' ); ?>
+    <?php get_template_part( 'templates/content', 'page' ); ?>
+
+    <?php
+	if ( is_plugin_active( 'cxseries/index.php' ) ) {
+		CXSeries\CXSeriesPlugin\list_results();
+	} else {
+	?>
+      <p>The CX Series plugin is not activated!</p>
+    <?php
+	}
+	?>
 
 <?php endwhile;
--- templates/content-single-races.php
+++ templates/content-single-races.php
@@ -1,18 +1,33 @@
-<?php while (have_posts()) : the_post(); ?>
+<?php
+while ( have_posts() ) :
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	the_post();
+?>
   <article <?php post_class(); ?>>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php //get_template_part('templates/entry-meta'); ?>
+        <?php // get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
-      <div class="cxseries_race_details">
-        <?php cxseries_display_race_details(false); ?>
-      </div>
-      <?php the_content(); ?>
+        <?php
+		if ( is_plugin_active( 'cxseries/index.php' ) ) {
+		?>
+            <div class="cxseries_race_details">
+            <?php CXSeries\CXSeriesPlugin\display_race_details( false ); ?>
+          </div>
+        <?php
+		} else {
+		?>
+          <p>The CX Series plugin is not activated!</p>
+        <?php
+		}
+		?>
+
+        <?php the_content(); ?>
     </div>
     <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+        <?php wp_link_pages( [ 'before' => '<nav class="page-nav"><p>' . __( 'Pages:', 'sage' ), 'after' => '</p></nav>' ] ); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+    <?php comments_template( '/templates/comments.php' ); ?>
   </article>
 <?php endwhile; ?>
