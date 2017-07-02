e53b8819b84fdf6566cbeba5b899b4d0aa15ae55
Ben Wilson <ben@thelocust.org>
2017-07-02 17:10:34 -0400

    various Bootstrap 4 updates
    - new nav walker to support bs4 nomenclature (nav-item instead of
    menu-item)
    https://gist.github.com/smutek/cd95c8bc4f1db70ee1eda2740bfbf6fd

--- app/filters.php
+++ app/filters.php
@@ -32,65 +32,82 @@ add_filter('excerpt_more', function () {
 	return ' &hellip; <a href="' . get_permalink() . '">' . __( 'Continued', 'sage' ) . '</a>';
 });
 
+
 /**
+ * allow sidebar on homepage
+ */
+	add_filter('sage/display_sidebar', function ( $display ) {
+		static $display;
+		isset( $display ) || $display = in_array(true, [
+			// The sidebar will be displayed if any of the following return true
+			is_front_page(),
+			is_page(),
+			is_single(),
+			is_404(),
+			is_page_template( 'template-custom.php' ),
+		]);
+		return $display;
+	});
+
+	/**
  * Template Hierarchy should search for .blade.php files
  */
-collect([
-	'index',
-	'404',
-	'archive',
-	'author',
-	'category',
-	'tag',
-	'taxonomy',
-	'date',
-	'home',
-	'frontpage',
-	'page',
-	'paged',
-	'search',
-	'single',
-	'singular',
-	'attachment',
-])->map(function ( $type ) {
-	add_filter("{$type}_template_hierarchy", function ( $templates ) {
-		return collect( $templates )->flatMap(function ( $template ) {
-			$transforms = [
+	collect([
+		'index',
+		'404',
+		'archive',
+		'author',
+		'category',
+		'tag',
+		'taxonomy',
+		'date',
+		'home',
+		'frontpage',
+		'page',
+		'paged',
+		'search',
+		'single',
+		'singular',
+		'attachment',
+	])->map(function ( $type ) {
+		add_filter("{$type}_template_hierarchy", function ( $templates ) {
+			return collect( $templates )->flatMap(function ( $template ) {
+				$transforms = [
 				'%^/?(resources[\\/]views)?[\\/]?%' => '',
 				'%(\.blade)?(\.php)?$%' => '',
-			];
-			$normalizedTemplate = preg_replace( array_keys( $transforms ), array_values( $transforms ), $template );
-			return [ "{$normalizedTemplate}.blade.php", "{$normalizedTemplate}.php" ];
-		})->toArray();
+				];
+				$normalizedTemplate = preg_replace( array_keys( $transforms ), array_values( $transforms ), $template );
+				return [ "{$normalizedTemplate}.blade.php", "{$normalizedTemplate}.php" ];
+			})->toArray();
+		});
 	});
-});
 
-/**
+	/**
  * Render page using Blade
  */
-add_filter('template_include', function ( $template ) {
-	$data = collect( get_body_class() )->reduce(function ( $data, $class ) use ( $template ) {
-		return apply_filters( "sage/template/{$class}/data", $data, $template );
-	}, []);
-	echo template( $template, $data );
-	// Return a blank file to make WordPress happy
-	return get_theme_file_path( 'index.php' );
-}, PHP_INT_MAX);
+	add_filter('template_include', function ( $template ) {
+		$data = collect( get_body_class() )->reduce(function ( $data, $class ) use ( $template ) {
+			return apply_filters( "sage/template/{$class}/data", $data, $template );
+		}, []);
+		echo template( $template, $data );
+		// Return a blank file to make WordPress happy
+		return get_theme_file_path( 'index.php' );
+	}, PHP_INT_MAX);
 
-/**
+	/**
  * Tell WordPress how to find the compiled path of comments.blade.php
  */
-add_filter( 'comments_template', 'App\\template_path' );
+	add_filter( 'comments_template', 'App\\template_path' );
 
-/**
- *
- * Add "active" class for bootstrap where current_page_item is applied in WP
- */
-function nav_menu_css_class( $classes, $item ) {
-	if ( in_array( 'current-menu-item', $classes ) ) {
-		$classes[] = 'active ';
+	/**
+	 *
+	 * Add "active" class for bootstrap where current_page_item is applied in WP
+	 */
+	function nav_menu_css_class( $classes, $item ) {
+		if ( in_array( 'current-menu-item', $classes ) ) {
+			$classes[] = 'active ';
+		}
+		return $classes;
 	}
-	return $classes;
-}
 
-add_filter( 'nav_menu_css_class', __NAMESPACE__ . '\\nav_menu_css_class', 10, 2 );
+	add_filter( 'nav_menu_css_class', __NAMESPACE__ . '\\nav_menu_css_class', 10, 2 );
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,24 +1,31 @@
-<header class="banner">
-  <div class="container">
-    <?php /*    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a> */ ?>
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
 
-    <?php if ( get_theme_mod( 'cxseries_logo' ) ) : ?>
-        <div class='site-logo'>
-            <a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img style="max-height: <?php echo get_theme_mod( 'cxseries_logo_height' ) ?>px" src='<?php echo esc_url( get_theme_mod( 'cxseries_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
-        </div>
-        <h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+          aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <?php if (get_theme_mod('cxseries_logo')) : ?>
+      <div class='site-logo'>
+          <a href='<?php echo esc_url(home_url('/')); ?>' title='<?php echo esc_attr(get_bloginfo('name', 'display')); ?>' rel='home'><img style="max-height: <?php echo get_theme_mod('cxseries_logo_height') ?>px" src='<?php echo esc_url(get_theme_mod('cxseries_logo')); ?>' alt='<?php echo esc_attr(get_bloginfo('name', 'display')); ?>'></a>
+      </div>
+      <h1 class='site-title'><a href='<?php echo esc_url(home_url('/')); ?>' title='<?php echo esc_attr(get_bloginfo('name', 'display')); ?>' rel='home'><?php bloginfo('name'); ?></a></h1>
 
     <?php else : ?>
-        <hgroup>
-            <h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
-            <h2 class='site-description'><?php bloginfo( 'description' ); ?></h2>
-        </hgroup>
+      <hgroup>
+          <h1 class='site-title'><a href='<?php echo esc_url(home_url('/')); ?>' title='<?php echo esc_attr(get_bloginfo('name', 'display')); ?>' rel='home'><?php bloginfo('name'); ?></a></h1>
+          <h2 class='site-description'><?php bloginfo('description'); ?></h2>
+      </hgroup>
     <?php endif; ?>
 
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-tabs']) !!}
-      @endif
-    </nav>
+  <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav mr-auto']) !!}
+    @endif
   </div>
-</header>
+</nav>
